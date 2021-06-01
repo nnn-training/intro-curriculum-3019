cc2e5432547db7e0753eac318873e7228fbbf918
ky0yk <ksaik1024@gmail.com>
2021-06-01 21:39:09 +0900

    リダイレクトを実装

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 const http = require('http');
+const redirectUrl = 'https://www.nicovideo.jp/';
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    Location: redirectUrl
+  });
   res.end();
 });
 const port = 8000;
