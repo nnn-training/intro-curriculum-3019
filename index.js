d968a87e47426ab802f8250e5c5a7368db47b5cc
risarisato <nqg44226@gmail.com>
2021-10-26 21:16:34 +0900

    2020年アーカイブでリポジトリ削除したので再テスト

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  // ニコニコ動画へ
+  res.writeHead(302, {
+    'Location': 'https://www.nicovideo.jp/'
+  });
   res.end();
 });
 const port = 8000;
