6d20eb2578286db8469a871e4efda1dd7cb69580
Hishiya <harukihh4@gmail.com>
2021-10-29 19:34:26 +0900

    リダイレクト処理の追加

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+const redirect_URL = 'https://www.nicovideo.jp/'
+  res.writeHead(302, {
+    Location: redirect_URL
+  })
   res.end();
 });
 const port = 8000;
